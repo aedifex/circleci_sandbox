75d2f7b2029d4542a4a1e83c7de9206d09eea75c
Chris B <chris@circleci.com>
2018-06-08 16:57:26 -0700

    testing

--- .circleci/sample_script.sh
+++ .circleci/sample_script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+CIRCLE_URL="https://circleci.nike.com"
+NT_ID="mreyn9"
+PROJECT_NAME="gogogo"
+CIRCLE_TOKEN=$(cat ~/.circle-token)
+AWS_ACCESS_KEY=$(cat ~/.aws.access.key)
+AWS_SECRET_KEY=$(cat ~/.aws.secret.key)
+cat > tmp-body <<BODY
+{"aws": {"keypair": {"access_key_id": "${AWS_ACCESS_KEY}","secret_access_key": "${AWS_SECRET_KEY}"}}}
+BODY
+# curl -X PUT -H 'content-type: application/json' -d tmp-body ${CIRCLE_URL}/api/v1.1/project/github/${NT_ID}/${PROJECT_NAME}/settings?circle-token=${CIRCLE_TOKEN}
 
-curl -vvv -s -X PUT -d "`cat aglet_v.yaml`" -H "X-Aglet-Token: \"${AGLETPREPRODTOKEN}\"" -k "http://httpbin.org/put"
+curl -X PUT -H 'content-type: application/json' -d @tmp-body http://httpbin.org/put
